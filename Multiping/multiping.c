4b113d6a8862cf3620f36dd629dc35cf0323c842
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2019-11-17 11:59:38 +0900

    Add files via upload

---
+++ Multiping/multiping.c
@@ -0,0 +1,126 @@
+/*--------------------------------------------------------------------------
+  Tool name  	:multiping
+  Module name   :multiping.c
+  Detail        :ping to multiple IP adress
+  Implementer   :R.Ishikawa
+  Version       :1.1
+  Last update   :2018/04/19
+ --------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------
+  Version history
+   1. Create new                          R.I     Ver.1.0   2018/04/13
+   2. Change variable to be readable      R.I     Ver.1.1   2018/04/19
+ --------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#define BUFSIZE 64
+
+char *throwPing(char *);
+char *getTime( char *, char * );
+
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  int j = 0;
+  int intMax = 0;
+  int intCount[16];
+  char cmd[BUFSIZE];
+  char *strReturn;
+  time_t t;
+
+  if(argc < 2) {
+    printf(" multiping v1.1\n Syntax: mping <IPaddress1> <IPaddress2> ..\n" );
+    exit(-1);
+  }
+
+  intMax = argc - 1;
+  for(i = 0; i < 16; i++) intCount[i] = 0;
+  printf("-----------------------------------------------\n");
+  printf("A multiping targets %d IP address:\n", intMax);
+  printf("-----------------------------------------------\n");
+
+
+  do {
+    if(j == 0) {
+      time(&t);
+      printf(" %s", ctime(&t));
+      for(i = 0; i < intMax; i++) {
+        printf("%16s", argv[i+1]);
+      }
+      printf("\n");
+      for(i = 0; i < intMax; i++) {
+        printf("%16d", intCount[i+1]);
+      }
+      printf(" <- NG out of %d\n", intCount[0]);
+    }
+    if(j++ == 9) j = 0;
+
+    intCount[0]++;
+    for (i = 0; i < intMax; i++) {
+      snprintf(cmd, BUFSIZE, "ping -c 1 %s", argv[i+1]);
+      strReturn = throwPing(cmd);
+      if(strReturn != "NULL"){
+        //printf( "%16s", strReturn );
+        printf("      *** OK ***");
+      } else {
+        printf("      *** NG ***");
+        intCount[i+1]++;
+      }
+    }
+    printf("\n");
+    sleep(1);
+  } while(1);
+
+  return 0;
+}
+
+ 
+char *throwPing(char *strCmd)
+{
+  FILE *fp;
+  char buf[BUFSIZE];
+  char *token;
+
+    if((fp = popen(strCmd, "r")) == NULL) {
+      printf("Can not exec ping. ");
+      return "NULL";
+    }
+    fgets(buf, BUFSIZE, fp);
+    fgets(buf, BUFSIZE, fp);
+    pclose(fp);
+
+    if(strstr(buf, "ttl=") != NULL) {
+      token = getTime(buf, "=");
+      return token;
+    } else {
+      return "NULL";
+    }
+
+  return "NULL";
+}
+
+char *getTime(char strTest[BUFSIZE], char strTrim[BUFSIZE])
+{
+  char *token;
+  unsigned int len;
+
+  token = strtok(strTest, strTrim);
+
+  while( token != NULL ) {
+    if(strstr(token,"ms")){
+        len = strlen(token);
+        token[len-1] = '\0';
+        return token;
+    }
+    token = strtok(NULL, strTrim);
+  }
+  return "NULL";
+}
+
+
